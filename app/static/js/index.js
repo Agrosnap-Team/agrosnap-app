c954a2b92835ddb5bfa19e2a0e5fbfeef42407e1
Aseel khanfar <57755284+Aseelkhanfar19@users.noreply.github.com>
2026-07-01 22:00:43 +0300

    add animation to logo transfer to sign page change file name

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,10 +1,13 @@
+
+
 function display_Agrosnap_logo(){
-
-
-
+document.getElementById("logo_container").classList.add("fade-in");
+setTimeout(move_to_sign_page, 4000);
 
 }
 
 function move_to_sign_page(){
+
+    window.location.href = "../templates/sign_choice.html";
 
 }
