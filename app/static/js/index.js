d2e052d78e17fb4b30ea227990e6e24c22dd7400
Aseel khanfar <57755284+Aseelkhanfar19@users.noreply.github.com>
2026-07-01 22:29:27 +0300

    modified the animation

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,13 +1,14 @@
 
 
 function display_Agrosnap_logo(){
-document.getElementById("logo_container").classList.add("fade-in");
-setTimeout(move_to_sign_page, 4000);
+    document.getElementById("logo_container").classList.add("fade-in");
+// 2. Professionally detect exactly when the animation ends
+    document.getElementById("logo_container").addEventListener("animationend", move_to_sign_page);
 
 }
 
 function move_to_sign_page(){
 
-    window.location.href = "../templates/sign_choice.html";
+    window.location.href = "sign_choices.html";
 
 }
