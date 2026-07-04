c57f682b66f424f2b8727ebd9471f9078d9cf08c
Aseel khanfar <57755284+Aseelkhanfar19@users.noreply.github.com>
2026-07-05 02:00:34 +0300

    style: update CSS for sidebar and add all-reports styles; enhance dynamic page loading

--- app/static/js/dynamic_pages.js
+++ app/static/js/dynamic_pages.js
@@ -1,14 +1,66 @@
+//the user will click on the li 
+// we will take the index of li 
+//change the localStorage to that index
+// we pass it to fit_the_page()
+// we create an array of [pageName.html] to get them fast
+// we navigate the idex of li with array of pages to get the page name 
+// pass it to fetch()
+//change the background of clicked li 
+// if user logged out , the localStorage will reset the page to 0 which is the default page
 
-localStorage.setItem("current_page",0);
+
+const listItems = document.querySelectorAll('#pages li');
+
+// 2. Loop through each item
+listItems.forEach((item, index) => {
+  
+  // 3. Add the click listener to each individual item
+  item.addEventListener('click', () => {
+    localStorage.setItem("current_page",index);//put the index of li inside localStorage
+    current_page = localStorage.getItem("current_page");
+
+    fit_the_page(current_page);
+    highlightActiveTab(index);
+    document.getElementById("sidebar").classList.remove("open");
+    document.getElementById("sidebar").classList.add("close");
+    document.getElementById("overlay").classList.remove("show");
+    
+
+  });
+});
 
 
-async function fit_the_page() {
-    let the_url = fetch("scan.html")
+
+
+
+window.onload = function(){
+     
+
+     let saved_page = localStorage.getItem("current_page");
+
+     console.log(saved_page + " " +typeof(saved_page));
+    // Convert the string to a number. If it's the first visit (null), default to 0.
+    current_page = saved_page !== null ? parseInt(saved_page, 10) : 0;
+
+    fit_the_page(current_page);
+    highlightActiveTab(current_page);
+
+    
+}
+
+
+async function fit_the_page(curr_page) {
+
+    const all_pages = ["scan.html","all-saved-reports.html","help-center.html","about.html"];
+    var clicked_page = all_pages[curr_page]; //get the name of page
+
+    let the_url = fetch(clicked_page) //this method will go and search about this page
     .then(response => {
         return response.text();
     })
     .then(HTML_data =>{
-        document.getElementById("content").innerHTML=HTML_data;
+        document.getElementById("main").scrollTop=0; //this to let the page go to top again , if user scrolled down
+        document.getElementById("content").innerHTML=HTML_data; // put the new page here
     })
     .catch(error =>{
         console.error("something went wrong , " + error);
@@ -16,18 +68,16 @@ async function fit_the_page() {
 }
 
 
-window.onload = function(){
-    var current_page = localStorage.getItem("current_page");
+function highlightActiveTab(activeIndex){
 
-    if (current_page == 0){
-        fit_the_page();
+    //li's
+    listItems.forEach((li, index) => {
+        if (index === activeIndex) {
+            li.classList.add("active");// Highlight active tab
+        } else {
+            li.classList.remove("active");// Reset others
+        }
+    });
 
-    }
-    else{
-        console.log("the current page is " + current_page);
-    }
-    
-
-
-};
+}
 
