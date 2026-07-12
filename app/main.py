27572c06626f266d2b85cf9e93c7abdbce1e681f
Aseel khanfar <57755284+Aseelkhanfar19@users.noreply.github.com>
2026-07-12 21:05:39 +0300

    Refactor imports in main.py and add manifest.json for PWA support; update registration form to disable password autocomplete

--- app/main.py
+++ app/main.py
@@ -6,9 +6,8 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, EmailStr,field_validator,model_validator
 from app import database as db
-from fastapi import FastAPI, Request
-from fastapi.templating import Jinja2Templates
-from fastapi.responses import FileResponse,HTMLResponse
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
 import bcrypt
 import jwt
 from datetime import datetime ,timedelta,timezone
