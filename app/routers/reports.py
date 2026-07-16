b4dbf7991fe3777f1d0e5f95cdaaed06b294e6fe
user <talashrayre@gmail.com>
2026-07-17 01:58:41 +0300

    def save-report

--- app/routers/reports.py
+++ app/routers/reports.py
@@ -2,8 +2,17 @@
 
 from fastapi import APIRouter,HTTPException,status
 from app import database as db
+from app.routers.schema_classes import Data_to_save
+
 
 database_instance = db.AgrosnapDatabase()
 
 router = APIRouter()
+
+def Save_reports(data_to_save :Data_to_save):
+
+    try:
+
+
+
 
